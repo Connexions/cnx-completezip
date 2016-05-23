a19e5766dfb6a90bdc5a8896db6e143d56aed138
Michael Mulich <michael.mulich@gmail.com>
2016-05-23 09:10:29 -0700

    :tada: It's a package

---
+++ README.rst
@@ -0,0 +1,14 @@
+=======================
+CNX CompleteZip Library
+=======================
+
+This library is used for parsing, validating and creating completezip
+formatted content.
+
+
+License
+-------
+
+This software is subject to the provisions of the GNU Affero General
+Public License Version 3.0 (AGPL). See license.txt for details.
+Copyright (c) 2013 Rice University
---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup, find_packages
+
+
+IS_PY3 = sys.version_info > (3,)
+
+setup_requires = (
+    'pytest-runner',
+    )
+install_requires = (
+    'lxml',
+    )
+tests_require = [
+    'pytest',
+    ]
+extras_require = {
+    'test': tests_require,
+    }
+description = ""
+
+if not IS_PY3:
+    tests_require.append('mock==1.0.1')
+
+setup(
+    name='cnx-completezip',
+    version='0.1.0',
+    author='Connexions team',
+    author_email='info@cnx.org',
+    url="https://github.com/connexions/cnx-completezip",
+    license='LGPL, See also LICENSE.txt',
+    description=description,
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
+    test_suite='cnxcompletezip.tests',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        },
+    entry_points="""\
+    """,
+    )
